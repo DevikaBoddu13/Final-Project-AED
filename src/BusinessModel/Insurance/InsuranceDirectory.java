c36d8ad44212b660d569d910d54cd27f099c4aa5
nikhilreddypolepally <polepally.n@northeastern.edu>
2022-12-03 20:44:15 -0500

    adds insurence class

---
+++ src/BusinessModel/Insurance/InsuranceDirectory.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BusinessModel.Insurance;
+
+import BusinessModel.Patient.Patient;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author nikhilreddypolepally
+ */
+public class InsuranceDirectory {
+    
+    public InsuranceDirectory() {
+        insuranceRecord = new ArrayList<>();
+    }
+    
+    private ArrayList<Patient> insuranceRecord; 
+
+    public ArrayList<Patient> getInsuranceRecord() {
+        return insuranceRecord;
+    }
+
+    public void setInsuranceRecord(ArrayList<Patient> insuranceRecord) {
+        this.insuranceRecord = insuranceRecord;
+    }
+        public void addServedCustomers(Patient patient)
+    {
+      /*  for(Patient p:labRecordsList){
+        if(!labRecordsList.contains(patient)){
+        labRecordsList.add(patient);
+        }
+        else if(p==patient && p.getPharmaStatus().equals("Delivered")){
+        labRecordsList.add(p);
+        }
+        }*/
+        insuranceRecord.add(patient);
+    }  
+}
