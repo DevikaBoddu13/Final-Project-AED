cc65c64443b888fe8b5321f06647a2c2f26c21cf
nikhilreddypolepally <polepally.n@northeastern.edu>
2022-12-04 20:29:09 -0500

    adds classes for each role

---
+++ src/BusinessModel/Roles/Patient_role.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BusinessModel.Roles;
+
+import BusinessModel.Ecosystem;
+import BusinessModel.UserAccount.User;
+import MainUserInterface.Patient.PatientWorkAreaJPanel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author nikhilreddypolepally
+ */
+public class Patient_role extends Role{
+    
+
+    @Override
+     public JPanel createWorkArea(JPanel userProcessContainer, User user, Ecosystem business) {
+        return new PatientWorkAreaJPanel(userProcessContainer, user, business);
+    }      
+}
